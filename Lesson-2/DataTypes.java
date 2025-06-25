b3f3c13ee519c71526c38b0b24224081fbf5f8f1
Revau1 <singhairevant1@gmail.com>
2025-06-25 19:18:20 +0530

    initial commit

---
+++ Lesson-2/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("The Addition of A is B is:" +(a+b)+"\n");
+        
+        
+        System.out.println("The Subtraction of A is B is:" +(a-b));
+        
+        System.out.println("The Multiplication of A is B is:" +(a*b));
+        
+        System.out.println("The Division of A is B is:" +(a/b));
+
+        System.out.println("The Remainder of A is B is:" +(a%b));
+
+        System.out.println("Addition: "+(a+b)+" Subtraction: "+(a-b)+" Multiplication: "+(a*b)+" Division: "+(a/b));
+    }
+}
---
+++ Project/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+package Project;
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 20; // Second number
+
+        int sum = num1 + num2; 
+        int difference = num1 - num2; 
+        int multiplication = num1 * num2; 
+        int division = num1 / num2; 
+        System.out.println("The sum is: " + sum);
+        System.out.println("The difference is: " + difference);
+        System.out.println("The multiplication is: " + multiplication);
+        System.out.println("The division is: " + division);
+    }
+}
